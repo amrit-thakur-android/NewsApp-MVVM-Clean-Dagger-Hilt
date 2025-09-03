0d37b7f1ab28eafa6de99c0f6f985ab897cfd22f
amrit-spoton <athakur@spoton.com>
2025-09-03 23:12:19 +0530

    Improve overall domain.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -15,16 +15,10 @@ kotlin {
 }
 
 dependencies {
-    // Coroutines for async operations
     implementation(libs.coroutines.core)
-
-    // Dependency Injection
     implementation(libs.javax.inject)
-
-    // Paging
     implementation(libs.paging.common)
 
-    // Testing
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(libs.coroutines.test)
